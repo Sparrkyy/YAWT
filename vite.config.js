f297ce5980a4f55828a18e4f5b42305fdfca586b
Sparrkyy <ethancampbellhodges@gmail.com>
2026-02-18 13:42:52 -0500

    service worker

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: false,          // keep existing public/manifest.json
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,svg}'],
+      },
+    }),
+  ],
   base: '/YAWT/',
 })
